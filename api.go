77ce17281c321c123ea346cc0053e83dfb00c533
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-22 14:25:40 +0300

    Starts tutorial

---
+++ api.go
@@ -0,0 +1,6 @@
+package main
+
+// This will be the foundation for the implementation of the server
+type APIServer struct {
+	addr string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adammwaniki/mi-primera-api-de-golang
+
+go 1.22.2
